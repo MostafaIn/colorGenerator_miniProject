d0749e3a10722fba7219c4a10f1c4665da603f25
MostafaIn <mostafa.hazareh@integrify.fi>
2019-03-19 16:08:48 +0200

    I have done the second mini project

---
+++ colorGenScript.js
@@ -0,0 +1,73 @@
+const container = document.querySelector('.container');
+const displayBtn = document.querySelector('button');
+const inputNo = document.querySelector('input');
+
+
+
+function colorGenerator() {
+    container.innerHTML = " ";
+    for (let i = 0; i < inputNo.value; i++) {
+        let r = Math.floor(Math.random() * 256).toString(16).toUpperCase();
+        let g = Math.floor(Math.random() * 256).toString(16).toUpperCase();
+        let b = Math.floor(Math.random() * 256).toString(16).toUpperCase();
+        r = (r.length === 2) ? r : '0' + r;
+        g = (g.length === 2) ? g : '0' + g;
+        b = (b.length === 2) ? b : '0' + b;
+        const colorCode = `#${r}${g}${b}`;
+        const colorBox = document.createElement('div');
+        colorBox.setAttribute("class", "colorBox");
+        const copyColor = document.createElement('i');
+        copyColor.setAttribute("class", "far fa-copy");
+        copyColor.style.backgroundColor = colorCode;
+
+        colorBox.textContent = colorCode;
+        // copyColor.textContent = "copy";
+        colorBox.style.backgroundColor = colorCode;
+        container.appendChild(colorBox);
+        colorBox.appendChild(copyColor);
+
+        // const colorCode = document.createElement('h4');
+        copyColor.addEventListener('click', function () {
+            try {
+
+                var range = document.createRange();// create new range object
+                range.selectNodeContents(colorBox); // set range to encompass desired element text
+                var selection = window.getSelection(); // get Selection object from currently user selected text
+                selection.removeAllRanges(); // unselect any user selected text (if any)
+                selection.addRange(range);// add range to Selection object t;
+            }
+            catch (err) {
+                console.log(err);
+            }
+            document.execCommand("copy");
+        })
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for color copy button 
+// const copyBtn = document.querySelector('.colorData');
+// copyBtn.addEventListener("click", function () {
+//     // copyToClipboard();
+//     // const colorData = document.createElement("textarea");
+//     copyBtn.value = colorCode;
+//     document.body.appendChild(copyBtn);
+//     copyBtn.select();
+//     document.execCommand("copy");
+//     document.body.removeChild(copyBtn);
+// });
---
+++ readME.md
@@ -0,0 +1,28 @@
+# Hexadecimal Color Generator
+
+> This is the second mini project of javascript tutorial in Integrify!
+
+##Demo
+
+[color generator App]()
+
+## Screenshot
+
+![Example screenshot](colorGenerator_screenshot.png)
+
+## Technologies
+
+- HTML
+- CSS
+- JavaScript
+
+## Status
+
+Project is: finished_,
+
+
+## Contact
+
+Created by [mostafa](https://github.mostafaIn.com) 
+
+![feel free to contact me!](mostafa.hazareh@integrify.io)
